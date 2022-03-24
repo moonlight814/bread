ce00c34f7dc8adbbcb4d170895bd0730ed2dfe97
Natalia <natyargumedo@hotmail.com>
2022-03-23 20:59:15 -0700

    added bootstrap and css and images

--- server.js
+++ server.js
@@ -5,19 +5,28 @@ require('dotenv').config()
 const PORT= process.env.PORT
 const app= express()
 
-//MIDDLEWARE
+
+// MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 //ROUTES
 app.get("/", (req, res) => {
     res.send("Wecome to an awesome app about breads")
 })
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+
 //BREADS
 const breadsController= require('./controllers/bread_controllers.js')
 app.use('/breads', breadsController)
+
 //Listen
 app.listen(PORT, ()=> {
     console.log('nomming at port', PORT)
