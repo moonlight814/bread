4cb6d678e669c33de7d0150feefd01c0dc461cb0
Natalia <natyargumedo@hotmail.com>
2022-03-30 20:30:17 -0700

    added mongoose package to include data. In the next commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express= require('express')
 const methodOverride= require('method-override')
+const mongoose = require('mongoose')
 
 //configuration
 require('dotenv').config()
 const PORT= process.env.PORT
 const app= express()
-
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    ()=> {console.log("connected to mongo")})
 
 // MIDDLEWARE
 app.use(express.static('public'))
